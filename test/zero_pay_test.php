609f4a31473600eb79980b5db481f506a88148d0
Lylla <72465355+sipaslyl@users.noreply.github.com>
2020-12-13 20:15:45 -0800

    Update zero_pay_test.php

--- test/zero_pay_test.php
+++ test/zero_pay_test.php
@@ -4,7 +4,7 @@ include_once 'includes/settings.php';
 require_once 'simpletest/autorun.php';
 require_once 'simpletest/web_tester.php';
 
-class HoursForm extends WebTestCase {
+class ZeroPayForm extends WebTestCase {
 
   function testZeroPay() {
 		  $this->get(VIRTUAL_PATH . '/hours.php');
