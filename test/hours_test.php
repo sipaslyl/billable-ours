8738e3a0d03a6e03888a128f3caa9b75875fb77b
Lylla <72465355+sipaslyl@users.noreply.github.com>
2020-12-13 20:00:00 -0800

    Update hours_test.php

--- test/hours_test.php
+++ test/hours_test.php
@@ -17,41 +17,5 @@ class HoursForm extends WebTestCase {
 		$this->assertResponse(200);
 		$this->assertText("You input 2 hours at a rate of $50 and your pay is $100");
 	}
-	
-	function testZeroHours() {
-		$this->get(VIRTUAL_PATH . '/hours.php');
-		$this->assertResponse(200);
-
-    $this->setField("hours", "0");
-		$this->setField("rate", "50");
-		$this->clickSubmit("Show Pay");
-
-		$this->assertResponse(200);
-		$this->assertText("You input 0 hours at a rate of $50 and your pay is $0");
-	}
-	
-	function testDecimalHours() {
-		$this->get(VIRTUAL_PATH . '/hours.php');
-		$this->assertResponse(200);
-
-    $this->setField("hours", "1.5");
-		$this->setField("rate", "50");
-		$this->clickSubmit("Show Pay");
-
-		$this->assertResponse(200);
-		$this->assertText("You input 1 hours at a rate of $50 and your pay is $50");
-	}
-	function testZeroPay() {
-		$this->get(VIRTUAL_PATH . '/hours.php');
-		$this->assertResponse(200);
-
-    $this->setField("hours", "2");
-		$this->setField("rate", "0");
-		$this->clickSubmit("Show Pay");
-
-		$this->assertResponse(200);
-		$this->assertText("You input 2 hours at a rate of $0 and your pay is $0");
-	}
-	
 
 }
